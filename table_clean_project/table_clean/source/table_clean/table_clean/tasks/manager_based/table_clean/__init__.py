1e6af87a03c2df48ada8930adfffe4cd7ed89cc8
CollaalloC <collaalloc@foxmail.com>
2025-11-28 00:37:18 +0800

    更新注册环境 id 和入口

--- table_clean_project/table_clean/source/table_clean/table_clean/tasks/manager_based/table_clean/__init__.py
+++ table_clean_project/table_clean/source/table_clean/table_clean/tasks/manager_based/table_clean/__init__.py
@@ -1,3 +1,11 @@
+'''
+Author: CollaalloC collaalloc@foxmail.com
+Date: 2025-11-27 16:23:24
+LastEditors: CollaalloC collaalloc@foxmail.com
+LastEditTime: 2025-11-28 00:36:36
+FilePath: /table_clean/table_clean_project/table_clean/source/table_clean/table_clean/tasks/manager_based/table_clean/__init__.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 # Copyright (c) 2022-2025, The Isaac Lab Project Developers.
 # All rights reserved.
 #
@@ -13,11 +21,11 @@ from . import agents
 
 
 gym.register(
-    id="Template-Table-Clean-v0",
+    id="Isaac-Table-Clean-IK-Rel-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.table_clean_env_cfg:TableCleanEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.table_clean_env_cfg:TableCleanIKRelEnvCfg",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
         "skrl_amp_cfg_entry_point": f"{agents.__name__}:skrl_amp_cfg.yaml",
